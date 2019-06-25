46dff27e4cd303a85add2bfcc7af807c5082f12e
konradk89 <konrad.kurzawski@gmail.com>
2019-06-25 23:19:17 +0200

    IT async pool fixed

--- src/integration-test/java/com/kurzawsk/simple_bank/BaseRestApiIT.java
+++ src/integration-test/java/com/kurzawsk/simple_bank/BaseRestApiIT.java
@@ -1,13 +1,14 @@
 package com.kurzawsk.simple_bank;
 
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.atomic.AtomicLong;
-
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 
-import io.restassured.specification.RequestSpecification;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
 
 public class BaseRestApiIT {
 
@@ -15,11 +16,12 @@ public class BaseRestApiIT {
     private static final String VERSION = "v1.0";
     private static final String BASE_URI = "http://localhost:8090";
     private static final AtomicLong counter = new AtomicLong(0L);
+    private static final Executor pool = Executors.newSingleThreadExecutor();
 
     @BeforeAll
     public static void setUp() {
         spec = RestApiTestHelper.createSpec(BASE_URI).basePath(VERSION);
-        CompletableFuture.runAsync(() -> Application.startService());
+        CompletableFuture.runAsync(Application::startService, pool);
     }
 
     @AfterAll
